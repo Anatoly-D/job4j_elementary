5f54c7b8a746709e10aae1699e2e88f331c285e0
Anatoly-D <Kalyazin69>
2020-07-12 18:29:14 +0300

    0. Что такое тестирование [#324807]

--- src/ru/job4j/calculator/Converter.java
+++ src/ru/job4j/calculator/Converter.java
@@ -3,17 +3,22 @@ package ru.job4j.calculator;
 public class Converter {
 
     public static void main(String[] args) {
-        float ruble = 200f;
-
+        int ruble = 560;
         System.out.println("in " + ruble + " RUB " + Converter.rublesToUSD(ruble) + " USD");
         System.out.println("in " + ruble + " RUB " + Converter.rublesToEUR(ruble) + " EUR");
+
+        int outUSD = 8;
+        int outEUR = 7;
+
+        System.out.println("RUB to USD. Test is passed: " + (Converter.rublesToUSD(ruble) == outUSD));
+        System.out.println("RUB to EUR. Test is passed: " + (Converter.rublesToEUR(ruble) == outEUR));
     }
 
-    private static float rublesToEUR(float ruble) {
+    private static int rublesToEUR(int ruble) {
         return ruble / 80;
     }
 
-    private static float rublesToUSD(float ruble) {
+    private static int rublesToUSD(int ruble) {
         return ruble / 70;
     }
 }
