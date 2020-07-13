b1fd385c3ec533368759f0cda1fffbd0a5ae0a79
Anatoly-D <Kalyazin69>
2020-07-13 13:26:37 +0300

    5.6. Простые числа [#324824]

---
+++ test/ru/job4j/loop/PrimeNumberTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class PrimeNumberTest {
+    @Test
+    public void when5() {
+        int count = PrimeNumber.calc(5);
+        assertThat(count, is(3));
+    }
+
+    @Test
+    public void when11() {
+        int count = PrimeNumber.calc(11);
+        assertThat(count, is(5));
+    }
+
+    @Test
+    public void when2() {
+        int count = PrimeNumber.calc(2);
+        assertThat(count, is(1));
+    }
+
+    @Test
+    public void when10000() {
+        int count = PrimeNumber.calc(10000);
+        assertThat(count, is(1229));
+    }
+
+}
