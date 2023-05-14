c4ccdb8023549583ed83c5738ddbb525f170192e
Anatoly-D <dav119@mail.ru>
2023-05-14 09:20:32 +0300

    9.3. Математические операторы и составные операторы присваивания [#504905]

--- src/main/java/ru/job4j/Echo.java
+++ src/main/java/ru/job4j/Echo.java
@@ -1,4 +1,13 @@
 package ru.job4j;
 
 public class Echo {
+    public static void main(String[] args) {
+        int a = 5;
+        float b = 3.4F;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(a + b);
+        b += a;
+        System.out.println(b);
+    }
 }
