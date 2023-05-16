f88b2a694505fa5eedcbb9300b3e3f9456a2de31
Anatoly-D <dav119@mail.ru>
2023-05-16 08:33:05 +0300

    9.3. Математические операторы и составные операторы присваивания [#504905] moved StringRunner.java to condition package

--- src/main/java/ru/job4j/Echo.java
+++ src/main/java/ru/job4j/Echo.java
@@ -2,12 +2,8 @@ package ru.job4j;
 
 public class Echo {
     public static void main(String[] args) {
-        int a = 5;
-        float b = 3.4F;
-        System.out.println(a);
-        System.out.println(b);
-        System.out.println(a + b);
-        b += a;
-        System.out.println(b);
+        for (int i = 0; i < 1000; i++) {
+            System.out.println(i + "   " + (char) i);
+        }
     }
 }
