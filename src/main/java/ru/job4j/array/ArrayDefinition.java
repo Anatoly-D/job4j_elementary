e73a88e82bbf135977f2a3c8af1dfe0eff93c26d
Anatoly-D <dav119@mail.ru>
2023-09-21 10:15:15 +0300

    6.0.3. Заполнение массива. [#226300 #144293]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,15 @@ public class ArrayDefinition {
         System.out.println("Size of ages: " + ages.length);
         System.out.println("Size of surnames: " + surnames.length);
         System.out.println("Size of prices: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Anatoly";
+        names[1] = "Sergey";
+        names[2] = "Dima";
+        names[3] = "Katya";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
